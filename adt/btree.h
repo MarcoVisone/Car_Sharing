49fd357d3e3bd18834d0e115e274b4029ae82068
Marco <marcovisone5@gmail.com>
2025-05-06 17:32:59 +0200

    Definizione di interfaccia btree.h

--- adt/btree.h
+++ adt/btree.h
@@ -5,4 +5,11 @@
 #ifndef BTREE_H
 #define BTREE_H
 
+typedef struct node* AVLIntervalTree;
+
+AVLIntervalTree avl_insert(AVLIntervalTree root, int key);
+AVLIntervalTree avl_delete(AVLIntervalTree root, int key);
+void avl_preorder(AVLIntervalTree root);
+void avl_inorder(AVLIntervalTree root);
+void avl_free(AVLIntervalTree root);
 #endif //BTREE_H
