7a3e8032479622d5aa832c8f1e250121a70b6728
Manuel. <russo.nello.manuel@gmail.com>
2025-05-06 18:20:49 +0200

    Aggiunti prototipi di funzioni a veicolo.h: - Getter e setter per targa, modello, descrizione, tariffa, prenotazioni - Funzione veicoloToString per ottenere una rappresentazione del veicolo in formato stringa - Funzione confrontaTarghe per confronto tra veicolo e targa

--- strutture_dati/veicolo.h
+++ strutture_dati/veicolo.h
@@ -1,8 +1,35 @@
-//
-// Created by marco on 5/6/25.
-//
+
 
 #ifndef VEICOLO_H
 #define VEICOLO_H
+
+#include "adt/btree.h"
+#include "strutture_dati/byte.h"
+
+typedef struct veicolo *veicolo;
+
+char* getTarga(veicolo *v);
+
+void setTarga(veicolo *v, char *targa);
+
+char* getModello(veicolo *v);
+
+void setModello(veicolo *v, char *modello);
+
+char* getDescrizione(veicolo *v);
+
+void setDescrizione(veicolo *v, char *descrizione);
+
+double getTariffa(veicolo *v);
+
+void setTariffa(veicolo *v, double tariffa);
+
+AVLIntervalTree getPrenotazione(veicolo *v);
+
+void setPrenotazione(veicolo *v, AVLIntervalTree prenotazione);
+
+Byte confrontaTarghe(veicolo *v, char *targa);
+
+char* veicoloToString(veicolo *v);
 
 #endif //VEICOLO_H
