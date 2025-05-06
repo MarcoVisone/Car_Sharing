a6c49ffa3d3089edc756315699e0a3864b2d3423
Marco <marcovisone5@gmail.com>
2025-05-06 17:29:16 +0200

    Aggiunta della struttura dati Prenotazione e delle relative funzioni
    Introdotta la struttura Prenotazione con metodi di creazione, distruzione e accesso. Questo aggiornamento include anche le utilità per la gestione dei campi cliente, intervallo e costo. Migliora la modularità e la riusabilità del modulo Prenotazione.

--- strutture_dati/prenotazione.h
+++ strutture_dati/prenotazione.h
@@ -4,5 +4,24 @@
 
 #ifndef PRENOTAZIONE_H
 #define PRENOTAZIONE_H
+#include "strutture_dati/intervallo.h"
+#include "strutture_dati/utente.h"
+typedef struct prenotazione *Prenotazione;
+
+Prenotazione prenotazioneCrea(Utente cliente, Intervallo i, double costo);
+
+void prenotazioneDestroy(Prenotazione p);
+
+Utente getCliente(Prenotazione p);
+
+Intervallo getDateIntervallo(Prenotazione p);
+
+double getCosto(Prenotazione p);
+
+void setCliente(Prenotazione p, Utente cliente);
+
+void setIntervallo(Prenotazione p, Intervallo i);
+
+void setCosto(Prenotazione p, double costo);
 
 #endif //PRENOTAZIONE_H
