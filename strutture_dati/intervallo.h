0355567ba463a17a4adae807f9657cb3b668d20e
Marco <marcovisone5@gmail.com>
2025-05-06 16:04:10 +0200

    Definizione di interfaccia Intervallo

--- strutture_dati/intervallo.h
+++ strutture_dati/intervallo.h
@@ -4,5 +4,21 @@
 
 #ifndef INTERVALLO_H
 #define INTERVALLO_H
+#include <time.h>
+#include "strutture_dati/byte.h"
+
+typedef struct intervallo* Intervallo;
+
+Intervallo intervalloCrea(time_t inizio, time_t fine);
+
+void intervalloDistruggi(Intervallo i);
+
+time_t intervalloInizio(Intervallo i);
+
+time_t intervalloFine(Intervallo i);
+
+Byte intervalloContenutoIn(Intervallo interno, Intervallo esterno);
+
+char *intervalloToString(Intervallo i);
 
 #endif //INTERVALLO_H
