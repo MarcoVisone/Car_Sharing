046ccb3e2762015e7e0cf21e19acde40ef584937
Manuel. <russo.nello.manuel@gmail.com>
2025-05-08 22:50:40 +0200

    Sviluppo funzione crea_veicolo. Sviluppo funzione distruggi_veicolo. Modificate funzioni di set con strncpy al posto di strcpy. Sviluppo funzione veicolo_in_stringa Aggiunta define MAX_LUNGHEZZA_TARIFFA

--- src/strutture_dati/veicolo.c
+++ src/strutture_dati/veicolo.c
@@ -1,63 +1,105 @@
-
 #include "../../include/strutture_dati/veicolo.h"
 #include <string.h>
 
-#define NUM_CARATTERI_TARGA 7
-#define MAX_LUNGHEZZA_MODELLO 50
-#define MAX_LUNGHEZZA_DESCRIZIONE 1024
+#define NUM_CARATTERI_TARGA 7 + 1
+#define MAX_LUNGHEZZA_MODELLO 50 + 1
+#define MAX_LUNGHEZZA_DESCRIZIONE 1024 + 1
+#define MAX_LUNGHEZZA_TARIFFA 9 + 1
 
 struct veicolo{
     char targa[NUM_CARATTERI_TARGA];
     char modello[MAX_LUNGHEZZA_MODELLO];
     char descrizione[MAX_LUNGHEZZA_DESCRIZIONE];
     double tariffa;
-    AVLIntervalTree prenotazione;
+    Prenotazioni prenotazioni;
 };
 
-char* getTarga(veicolo *v){
+Veicolo crea_veicolo(char *targa, char *modello, char *descrizione, double tariffa, Prenotazioni prenotazioni){
+    Veicolo v = malloc(sizeof(struct veicolo));
+    if(v == NULL) return NULL;
+
+    strncpy(v->targa, targa, NUM_CARATTERI_TARGA- 1);
+    v->targa[NUM_CARATTERI_TARGA-1] = '\0';
+
+    strncpy(v->modello, modello, MAX_LUNGHEZZA_MODELLO-1);
+    v->modello[MAX_LUNGHEZZA_MODELLO-1] = '\0';
+
+
+    strncpy(v->descrizione, descrizione, MAX_LUNGHEZZA_DESCRIZIONE-1);
+    v->descrizione[MAX_LUNGHEZZA_DESCRIZIONE-1] = '\0';
+
+    v->tariffa = tariffa;
+
+    v->prenotazioni = prenotazioni;
+
+    return v;
+}
+
+void distruggi_veicolo(Veicolo v){
+    distruggi_prenotazioni(v->prenotazioni);
+    free(v);
+}
+
+char* get_targa(Veicolo *v){
     return v->targa;
 }
 
-void setTarga(veicolo *v, char *targa){
-    strcpy(v->targa,targa);
+void set_targa(Veicolo *v, char *targa){
+    strncpy(v->targa, targa, NUM_CARATTERI_TARGA- 1);
+    v->targa[NUM_CARATTERI_TARGA-1] = '\0';
 }
 
-char* getModello(veicolo *v){
+char* get_modello(Veicolo *v){
     return v->modello;
 }
 
-void setModello(veicolo *v, char *modello){
-    strcpy(v->modello, modello);
+void set_modello(Veicolo *v, char *modello){
+    strncpy(v->modello, modello, MAX_LUNGHEZZA_MODELLO-1);
+    v->modello[MAX_LUNGHEZZA_MODELLO-1] = '\0';
 }
 
-char* getDescrizione(veicolo *v){
+char* get_descrizione(Veicolo *v){
     return v->descrizione;
 }
 
-void setDescrizione(veicolo *v, char *descrizione){
-    strcpy(v->descrizione, descrizione);
+void set_descrizione(Veicolo *v, char *descrizione){
+    strncpy(v->descrizione, descrizione, MAX_LUNGHEZZA_DESCRIZIONE-1);
+    v->descrizione[MAX_LUNGHEZZA_DESCRIZIONE-1] = '\0';
 }
 
-double getTariffa(veicolo *v){
+double get_tariffa(Veicolo *v){
     return v->tariffa;
 }
 
-void setTariffa(veicolo *v, double tariffa){
+void set_tariffa(Veicolo *v, double tariffa){
     v->tariffa = tariffa;
 }
 
-AVLIntervalTree getPrenotazione(veicolo *v){
-    return v->prenotazione;
+Prenotazioni get_prenotazioni(Veicolo *v){
+    return v->prenotazioni;
 }
 
-void setPrenotazione(veicolo *v, AVLIntervalTree p){
-    v->prenotazione = p;
+void set_prenotazioni(Veicolo *v, Prenotazioni prenotazioni){
+    v->prenotazioni = prenotazioni;
 }
 
-Byte confrontaTarghe(veicolo *v, char *targa){
-    if(strcmp(targa,v->targa) == 0){
-        return 1;
-    }
-    return 0;
+Byte confronta_targhe(Veicolo *v, char *targa){
+    return (strcmp(targa,v->targa) == 0);
 }
 
+char* veicolo_in_stringa(Veicolo *v){
+    int size = NUM_CARATTERI_TARGA +
+               MAX_LUNGHEZZA_MODELLO +
+               MAX_LUNGHEZZA_TARIFFA +
+               MAX_LUNGHEZZA_DESCRIZIONE + 100 + 1;
+
+    char buffer[size];
+
+    snprintf(buffer, size, "Modello: %s\nDescrizione: %s\nTarga: %s\nTariffa: %0.2lf",
+            v->modello,
+            v->descrizione,
+            v->targa,
+            v->tariffa);
+
+    return buffer;
+}
