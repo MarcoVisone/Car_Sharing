2a65b3e1dc3b54527417fb96844b7963c25e3d9f
a.sicignano <sicignanoantonio001@gmail.com>
2025-05-09 20:45:24 +0200

    data.h aggiunte strutture: data e storico; aggiunte runzioni ottieni_storico; aggiungi_storico; rimuovi_storico; ottieni_frequenza; azzera;frequenta;

--- include/strutture_dati/data.h
+++ include/strutture_dati/data.h
@@ -1,8 +1,18 @@
-//
-// Created by marco on 5/6/25.
-//
+/*
+ * Autore: Antonio Sicignano
+ * Data: 09/05/2025
+ */
 
 #ifndef DATA_H
 #define DATA_H
+#include "include/adt/lista_prenotazione.h"
 
+typedef struct data *Data;
+typedef struct storico *Storico;
+
+ListaPre ottieni_storico(Data data);
+ListaPre aggiungi_a_storico(Data data, Prenotazione prenotazione);
+ListaPre rimuovi_da_storico(Data data, Prenotazione prenotazione);
+int ottieni_frequenza(Data data);
+void azzera_frequenza(Data data);
 #endif //DATA_H
