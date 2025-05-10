a0ebca9329f2d3eb5e06a94839ac651bedfe23d4
Manuel. <russo.nello.manuel@gmail.com>
2025-05-10 19:13:20 +0200

    Sviluppate le funzioni crea_tabella_veicoli, distruggi_tabella_veicoli, aggiungi_veicolo_in_tabella, cerca_veicolo_in_tabella, rimuovi_veicolo_in_tabella, distruggi_veicolo_t

---
+++ src/strutture_dati/tabella_veicoli.c
@@ -0,0 +1,48 @@
+/*
+ * Autore: Russo Nello Manuel
+ * Data: 10/05/2025
+ */
+
+#include "strutture_dati/tabella_veicoli.h"
+
+TabellaVeicoli crea_tabella_veicoli(unsigned int grandezza){
+    return nuova_tabella_hash(grandezza);
+}
+
+static void distruggi_veicolo_t(void *veicolo){
+    Veicolo v = (Veicolo)veicolo;
+    distruggi_veicolo(v);
+}
+
+void distruggi_tabella_veicoli(TabellaVeicoli tabella_veicoli){
+    if(tabella_veicoli == NULL){
+        return;
+    }
+
+    /* Viene passata la funzione distruggi_veicolo_t per rispettare il parametro
+     * che deve restituire un void e avere come parametro un puntatore void
+     */
+    distruggi_tabella(tabella_veicoli, distruggi_veicolo_t);
+}
+
+Byte aggiungi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, Veicolo veicolo){
+    if(tabella_veicoli == NULL){
+        return 0;
+    }
+
+    return inserisci_in_tabella(tabella_veicoli, ottieni_targa(veicolo), (Veicolo)veicolo);
+}
+
+Veicolo cerca_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa){
+    if(tabella_veicoli == NULL){
+        return NULL;
+    }
+    return (Veicolo)cerca_in_tabella(tabella_veicoli, ottieni_targa(targa));
+}
+
+Byte rimuovi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa){
+    if(tabella_veicoli == NULL){
+        return 0;
+    }
+    return cancella_dalla_tabella(tabella_veicoli, targa, distruggi_veicolo_t);
+}
