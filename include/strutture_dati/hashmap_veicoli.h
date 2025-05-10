acc10be8b79dd14b45595de40c27a171a00a2a5a
Manuel. <russo.nello.manuel@gmail.com>
2025-05-10 19:06:55 +0200

    MOdificato il tipo della variabile grandezza da int ad unsigned int

--- include/strutture_dati/hashmap_veicoli.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by marco on 5/6/25.
-//
-
-#ifndef HASHMAP_VEICOLI_H
-#define HASHMAP_VEICOLI_H
-
-#endif //HASHMAP_VEICOLI_H
---
+++ include/strutture_dati/tabella_veicoli.h
@@ -0,0 +1,24 @@
+/*
+ * Autore: Russo Nello Manuel
+ * Data: 10/05/2025
+ */
+
+#ifndef HASHMAP_VEICOLI_H
+#define HASHMAP_VEICOLI_H
+
+#include "strutture_dati/tabella_hash.h"
+#include "modello/veicolo.h"
+
+typedef TabellaHash TabellaVeicoli;
+
+TabellaVeicoli crea_tabella_veicoli(unsigned int grandezza);
+
+void distruggi_tabella_veicoli(TabellaVeicoli tabella_veicoli);
+
+Byte aggiungi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, Veicolo veicolo);
+
+Veicolo cerca_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa);
+
+Byte rimuovi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa);
+
+#endif //HASHMAP_VEICOLI_H
