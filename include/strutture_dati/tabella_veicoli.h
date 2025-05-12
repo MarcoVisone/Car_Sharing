8089e984a786a4037c3196c48b964c839e8a0854
Marco <marcovisone5@gmail.com>
2025-05-12 13:31:00 +0200

    Aggiunto prototipo ottieni_veicoli_disponibili, per la funzione che in base all'Intervallo restituisce tutti i veicoli disponibili

--- include/strutture_dati/tabella_veicoli.h
+++ include/strutture_dati/tabella_veicoli.h
@@ -9,6 +9,7 @@
 #include "modelli/byte.h"
 
 // Rinominata TabellaHash in TabellaVeicoli
+typedef struct intervallo *Intervallo;
 typedef struct tabella_hash *TabellaVeicoli;
 typedef struct veicolo *Veicolo;
 
@@ -21,5 +22,7 @@ Byte aggiungi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, Veicolo veicolo
 Veicolo cerca_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa);
 
 Byte rimuovi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa);
+
+Veicolo *ottieni_veicoli_disponibili(TabellaVeicoli tabella_veicoli, Intervallo intervallo);
 
 #endif //HASHMAP_VEICOLI_H
