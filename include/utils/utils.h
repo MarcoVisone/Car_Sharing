2ded50e2f822b144e11796a7ef6132963a439eb5
Marco <marcovisone5@gmail.com>
2025-05-13 17:59:55 +0200

    Aggiunti prototipi: genera_tabella_veicoli e my_strdup

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -1,6 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "strutture_dati/tabella_veicoli.h"
+
+
+TabellaVeicoli genera_tabella_veicoli(unsigned int numero_veicoli);
+
+char* my_strdup(const char* s);
 
 
 #endif //UTILS_H
