e615a9822f465fa0f4d75bb28ffe0e7deacf3bb8
Marco <marcovisone5@gmail.com>
2025-05-13 09:20:14 +0200

    Rimossa costante _POSIX_C_SOURCE, e aggiornata logica delle funzioni per rendere il programma più portabile

--- src/modelli/intervallo.c
+++ src/modelli/intervallo.c
@@ -3,8 +3,6 @@
  * Data: 06/05/2025
  */
 
-#define _POSIX_C_SOURCE 200809L
-
 #include "modelli/intervallo.h"
 #include <stdlib.h>
 #include <time.h>
@@ -12,6 +10,8 @@
 
 #define DIMENSIONE_BUFFER 64
 #define DIMENSIONE_BUFFER_FINALE (2 * DIMENSIONE_BUFFER + 5)
+
+static time_t converti_data_in_time_t(const char *data);
 
 struct intervallo{
     time_t inizio;
@@ -62,20 +62,6 @@ Byte intervalli_si_sovrappongono(Intervallo interno, Intervallo esterno){
     return (interno->inizio <= esterno->fine) && (esterno->inizio <= interno->fine);
 }
 
-time_t converti_data_in_time(int anno, int mese, int giorno, int ora, int minuti){
-    struct tm temp = {0};
-
-    temp.tm_year = anno - 1900;
-    temp.tm_mon = mese - 1;
-    temp.tm_mday = giorno;
-    temp.tm_hour = ora;
-    temp.tm_min = minuti;
-    temp.tm_sec = 0;
-    temp.tm_isdst = -1;
-
-    return mktime(&temp);
-}
-
 Intervallo duplica_intervallo(Intervallo p){
     if(p == NULL) return NULL;
 
@@ -90,28 +76,56 @@ Byte compara_intervalli(Intervallo a, Intervallo b) {
     return 0;
 }
 
+static time_t converti_data_in_time_t(const char *data){
+    struct tm tm = {0};
+
+    if(sscanf(data, "%d/%d/%d %d:%d",
+              &tm.tm_mday, &tm.tm_mon, &tm.tm_year,
+              &tm.tm_hour, &tm.tm_min) != 5)
+        return -1;
+
+    tm.tm_year -= 1900;
+    tm.tm_mon -= 1;
+
+    return mktime(&tm);
+}
+
+Intervallo converti_data_in_intervallo(const char *inizio, const char *fine){
+    time_t t_inizio = converti_data_in_time_t(inizio);
+    time_t t_fine = converti_data_in_time_t(fine);
+
+    if(t_inizio == -1 || t_fine == -1) return NULL;
+
+    Intervallo intervallo = crea_intervallo(t_inizio, t_fine);
+
+    return intervallo;
+}
+
 char *intervallo_in_stringa(Intervallo i) {
     if (i == NULL) {
         return NULL;
     }
 
-	/* Creazione di 3 buffer 2 per convertire la data in stringa e 1 per concatenare i due buffer */
     char buffer1[DIMENSIONE_BUFFER] = {0};
     char buffer2[DIMENSIONE_BUFFER] = {0};
-	char *buffer_end = malloc(DIMENSIONE_BUFFER_FINALE);
-    struct tm temp1, temp2;
-
-	/* Trasformazione delle date inzio e fine in stringa */
-    if (localtime_r(&(i->inizio), &temp1) == NULL || localtime_r(&(i->fine), &temp2) == NULL) {
-        return NULL;
-    }
-
-    strftime(buffer1, sizeof(buffer1), "%d/%m/%Y %H:%M", &temp1);
-    strftime(buffer2, sizeof(buffer2), "%d/%m/%Y %H:%M", &temp2);
-
+    char *buffer_end = malloc(DIMENSIONE_BUFFER_FINALE);
     if (buffer_end == NULL) {
         return NULL;
     }
+
+    struct tm *ptr1 = localtime(&(i->inizio));
+    struct tm *ptr2 = localtime(&(i->fine));
+
+    if (ptr1 == NULL || ptr2 == NULL) {
+        free(buffer_end);
+        return NULL;
+    }
+
+    struct tm temp1 = *ptr1;
+    struct tm temp2 = *ptr2;
+
+    strftime(buffer1, sizeof(buffer1), "%d/%m/%Y %H:%M", &temp1);
+    strftime(buffer2, sizeof(buffer2), "%d/%m/%Y %H:%M", &temp2);
 
     snprintf(buffer_end, DIMENSIONE_BUFFER_FINALE, "%s -> %s", buffer1, buffer2);
 
