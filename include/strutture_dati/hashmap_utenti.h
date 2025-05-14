179e4071a31124fe06d6a9899908a0d671b4d455
sicig <sicignanoantonio001@gmail.com>
2025-05-14 11:23:35 +0200

    create funzioni per l'hash map utenti e il relativo .h

--- include/strutture_dati/hashmap_utenti.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by marco on 5/6/25.
-//
-
-#ifndef HASHMAP_UTENTI_H
-#define HASHMAP_UTENTI_H
-
-#endif //HASHMAP_UTENTI_H
---
+++ src/strutture_dati/tabella_utenti.c
@@ -0,0 +1,42 @@
+/*
+ * Autore: Antonio Sicignano
+ * Data: 09/05/2025
+ */
+
+#include "strutture_dati/tabella_utenti.h"
+
+TabellaUtenti crea_tabella_utenti(unsigned int grandezza){
+  return nuova_tabella_hash(grandezza);
+}
+
+static void distruggi_utente_t(void *utente){
+  Utente u = (Utente)utente;
+  distruggi_utente(u);
+}
+
+void distruggi_tabella_utenti(TabellaUtenti tabella_utenti){
+  if(tabella_utenti == NULL){
+    return;
+  }
+
+Byte aggiungi_utente_in_tabella(TabellaUtenti tabella_utenti, Utente utente){
+  if(tabella_utenti == NULL){
+    return 0;
+  }
+
+  return inserisci_in_tabella(tabella_utenti, ottieni_email(utente)), (Utente)utente);
+}
+
+Utente cerca_utente_in_tabella(TabellaUtenti tabella_utenti, char *email){
+  if(tabella_utenti == NULL){
+    return NULL;
+  }
+  return (Utente)cerca_in_tabella(tabella_utenti, email);
+}
+
+Byte rimuovi_utente_in_tabella(TabellaUtenti tabella_utenti, char *email){
+  if(tabella_utenti == NULL){
+    return 0;
+  }
+  return cancella_dalla_tabella(tabella_utenti, email, distruggi_utente_t);
+}
