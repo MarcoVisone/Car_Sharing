e3b14f15257477dcc9616ccfb57d02615370ed59
Marco <marcovisone5@gmail.com>
2025-05-15 16:47:11 +0200

    Aggiunta funzione carica_prenotazione, che carica una prenotazione dal file binario

--- src/utils/gestore_file.c
+++ src/utils/gestore_file.c
@@ -10,6 +10,7 @@
 #include "modelli/utente.h"
 
 static void salva_prenotazione(FILE *fp, Prenotazione prenotazione);
+static Prenotazione carica_prenotazione(FILE *fp);
 static void salva_prenotazioni(FILE *fp, Prenotazioni prenotazioni);
 static Prenotazioni carica_prenotazioni(FILE *fp);
 
@@ -54,6 +55,30 @@ static void salva_prenotazione(FILE *fp, Prenotazione prenotazione){
     fwrite(&fine, sizeof(fine), 1, fp);
 }
 
+static Prenotazione carica_prenotazione(FILE *fp){
+    if(fp == NULL) return NULL;
+
+    unsigned int len;
+    fread(&len, sizeof(len), 1, fp);
+    char *cliente = malloc(sizeof(char) * len);
+    fread(cliente, sizeof(char), len, fp);
+
+    double costo;
+    fread(&costo, sizeof(costo), 1, fp);
+
+    time_t inizio;
+    time_t fine;
+    fread(&inizio, sizeof(inizio), 1, fp);
+    fread(&fine, sizeof(fine), 1, fp);
+    Intervallo i = crea_intervallo(inizio, fine);
+
+    Prenotazione p = crea_prenotazione(cliente, i, costo);
+
+    free(cliente);
+
+    return p;
+}
+
 static void salva_prenotazioni(FILE *fp, Prenotazioni prenotazioni) {
     if (fp == NULL || prenotazioni == NULL) return;
     unsigned int size = 0, i;
@@ -73,23 +98,9 @@ static Prenotazioni carica_prenotazioni(FILE *fp) {
     if (fread(&size, sizeof size, 1, fp) != 1) return NULL;
 
     Prenotazioni pren = crea_prenotazioni();
-    char nome[DIMENSIONE_NOME];
 
     for (i = 0; i < size; i++) {
-        unsigned int len;
-        fread(&len, sizeof(len), 1, fp);
-        fread(nome, sizeof(char), len, fp);
-
-        double costo;
-        fread(&costo, sizeof(costo), 1, fp);
-
-        time_t inizio, fine;
-        fread(&inizio, sizeof(inizio), 1, fp);
-        fread(&fine, sizeof(fine),   1, fp);
-
-        Intervallo iv = crea_intervallo(inizio, fine);
-        Prenotazione p = crea_prenotazione(nome, iv, costo);
-        pren = aggiungi_prenotazione(pren, p);
+        pren = aggiungi_prenotazione(pren, carica_prenotazione(fp));
     }
     return pren;
 }
