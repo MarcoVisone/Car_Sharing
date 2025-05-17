678954a9b142b8b6022a36209987f6a4e2e5d411
Marco <marcovisone5@gmail.com>
2025-05-17 10:55:44 +0200

    Aggiunto commento a Byte.h

--- include/modelli/byte.h
+++ include/modelli/byte.h
@@ -5,5 +5,14 @@
 
 #ifndef BYTE_H
 #define BYTE_H
+
+/*
+ * Definizione: Byte
+ * -----------------
+ * Tipo definito per rappresentare un valore numerico a 8 bit (signed).
+ * Viene utilizzato per evitare ambiguità e rendere esplicita l'intenzione
+ * di utilizzare valori di piccole dimensioni.
+ */
 typedef signed char Byte;
-#endif //BYTE_H
+
+#endif // BYTE_H
