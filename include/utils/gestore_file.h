44ce5c45b78a31bcfc29defd0765f6888a86f4df
Marco <marcovisone5@gmail.com>
2025-05-18 17:07:47 +0200

    Modificati prototipi carica_utenti e carica_veicoli, per il funzionamento del codice

--- include/utils/gestore_file.h
+++ include/utils/gestore_file.h
@@ -5,10 +5,10 @@ typedef struct utente *Utente;
 
 void salva_vettore_veicoli(const char *nome_file_veicolo, const char *nome_file_prenotazioni, Veicolo vettore[], unsigned int num_veicoli);
 
-int carica_vettore_veicoli(const char *nome_file_veicolo, const char *nome_file_prenotazioni, Veicolo *vettore, unsigned int *num_veicoli);
+Veicolo *carica_vettore_veicoli(const char *nome_file_veicolo, const char *nome_file_prenotazioni, unsigned int *num_veicoli);
 
 void salva_vettore_utenti(const char *nome_file_utente, const char *nome_file_data, Utente vettore[], unsigned int num_utenti);
 
-int carica_vettore_utenti(const char *nome_file_utente, const char *nome_file_data, Utente *vettore, unsigned int *num_utenti);
+Utente *carica_vettore_utenti(const char *nome_file_utente, const char *nome_file_data, unsigned int *num_utenti);
 
 #endif //GESTORE_FILE_H
