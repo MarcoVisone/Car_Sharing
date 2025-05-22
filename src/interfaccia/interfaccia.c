16fe4f5f83b1a4cf9f7d4aaff55388fb6d85496c
Manuel. <russo.nello.manuel@gmail.com>
2025-05-22 14:39:33 +0200

    Sviluppo funzione richiedi_intervallo_prenotazione

--- src/interfaccia/interfaccia.c
+++ src/interfaccia/interfaccia.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define DIMENSIONE_STRINGA_PASSWORD (64 + 1)
-
+#define DIMENSIONE_INTERVALLO (10 + 1)
 /*
  * Funzione: risposta_password
  * ---------------------------
@@ -31,26 +31,26 @@
  *    Output su console
  */
 static Byte risposta_password(Byte lvl){
-  switch (lvl){
-    case -1:
-      printf("La password deve avere:\n Almeno 8 caratteri\n Almeno una lettera minuscola ed una minuscola\n");
-      printf(" Almeno un numero\n Almeno un carattere speciale\n");
-      break;
+    switch (lvl){
+      case -1:
+        printf("La password deve avere:\n Almeno 8 caratteri\n Almeno una lettera minuscola ed una minuscola\n");
+        printf(" Almeno un numero\n Almeno un carattere speciale\n");
+        break;
 
-    case 0:
-      printf("Inserisci almeno un carattere speciale o un numero\n");
-      break;
+      case 0:
+        printf("Inserisci almeno un carattere speciale o un numero\n");
+        break;
 
-    case 1:
-      printf("Inserisci almeno un carattere speciale\n");
-      break;
+      case 1:
+        printf("Inserisci almeno un carattere speciale\n");
+        break;
 
-    default:
-      printf("PASSWORD VALIDA\n");
-      return 1;
-  }
+      default:
+        printf("PASSWORD VALIDA\n");
+        return 1;
+    }
 
-  return -1;
+    return -1;
 }
 
 /*
@@ -80,14 +80,14 @@ static Byte risposta_password(Byte lvl){
  *    Lettura da stdin
  */
 void ottieni_stringa(char *stringa, int dimensione){
-  fgets(stringa, dimensione, stdin);
-  while(*stringa){
-    if (*stringa == '\n'|| *stringa == ' '){
-      *stringa = '\0';
-      break;
+    fgets(stringa, dimensione, stdin);
+    while(*stringa){
+      if (*stringa == '\n'|| *stringa == ' '){
+        *stringa = '\0';
+        break;
+      }
+      stringa++;
     }
-    stringa++;
-  }
 }
 
 /*
@@ -117,38 +117,38 @@ void ottieni_stringa(char *stringa, int dimensione){
  *    Nessuno
  */
 Byte interfaccia_accesso(TabellaUtenti tabella_utenti){
-  char email[DIMENSIONE_EMAIL];
-  char password[DIMENSIONE_STRINGA_PASSWORD];
-  uint8_t password_mod[DIMENSIONE_PASSWORD];
-  Byte tentativi = 0;
-  Byte scelta;
+    char email[DIMENSIONE_EMAIL];
+    char password[DIMENSIONE_STRINGA_PASSWORD];
+    uint8_t password_mod[DIMENSIONE_PASSWORD];
+    Byte tentativi = 0;
+    Byte scelta;
 
-  Utente utente;
+    Utente utente;
 
-  do{
-    printf("Inserisci l'email: ");
-    ottieni_stringa(email, DIMENSIONE_EMAIL);
+    do{
+      printf("Inserisci l'email: ");
+      ottieni_stringa(email, DIMENSIONE_EMAIL);
 
-    printf("Inserisci la password: ");
-    ottieni_stringa(password, DIMENSIONE_STRINGA_PASSWORD);
-    md5(password, strlen(password), password_mod);
+      printf("Inserisci la password: ");
+      ottieni_stringa(password, DIMENSIONE_STRINGA_PASSWORD);
+      md5(password, strlen(password), password_mod);
 
-    utente = cerca_tabella_utenti(tabella_utenti, email);
+      utente = cerca_tabella_utenti(tabella_utenti, email);
 
-    if(utente && !hash_equals(ottieni_password(utente), password_mod, DIMENSIONE_PASSWORD)){
-      printf("Email o password errati\n");
+      if(utente && !hash_equals(ottieni_password(utente), password_mod, DIMENSIONE_PASSWORD)){
+        printf("Email o password errati\n");
 
-      printf("Vuoi uscire? (S/N): ");
-      scelta = getchar();
-      if(scelta == 's' || scelta == 'S'){
-        return -1;
+        printf("Vuoi uscire? (S/N): ");
+        scelta = getchar();
+        if(scelta == 's' || scelta == 'S'){
+          return -1;
+        }
+        tentativi += 1;
       }
-      tentativi += 1;
-    }
-    else break;
-  }while(tentativi < 3);
+      else break;
+    }while(tentativi < 3);
 
-  return 1;
+    return 1;
 }
 
 /*
@@ -180,46 +180,77 @@ Byte interfaccia_accesso(TabellaUtenti tabella_utenti){
  *    Aggiunta di un nuovo utente alla tabella utenti
  */
 Byte interfaccia_registrazione(TabellaUtenti tabella_utenti, Byte permesso){
-  char nome[DIMENSIONE_NOME];
-  char cognome[DIMENSIONE_COGNOME];
-  char email[DIMENSIONE_EMAIL];
-  char password[DIMENSIONE_STRINGA_PASSWORD];
-  char password_2[DIMENSIONE_STRINGA_PASSWORD];
-  uint8_t password_mod[DIMENSIONE_PASSWORD];
-  Utente utente;
-  Byte conferma;
-  Byte lvl;
+    char nome[DIMENSIONE_NOME];
+    char cognome[DIMENSIONE_COGNOME];
+    char email[DIMENSIONE_EMAIL];
+    char password[DIMENSIONE_STRINGA_PASSWORD];
+    char password_2[DIMENSIONE_STRINGA_PASSWORD];
+    uint8_t password_mod[DIMENSIONE_PASSWORD];
+    Utente utente;
+    Byte conferma;
+    Byte lvl;
 
-  printf("Inserisci il nome: ");
-  ottieni_stringa(nome, DIMENSIONE_NOME);
+    printf("Inserisci il nome: ");
+    ottieni_stringa(nome, DIMENSIONE_NOME);
 
-  printf("Inserisci il cognome: ");
-  ottieni_stringa(cognome, DIMENSIONE_COGNOME);
+    printf("Inserisci il cognome: ");
+    ottieni_stringa(cognome, DIMENSIONE_COGNOME);
 
-  printf("Inserisci l'email: ");
-  ottieni_stringa(email, DIMENSIONE_EMAIL);
+    printf("Inserisci l'email: ");
+    ottieni_stringa(email, DIMENSIONE_EMAIL);
 
-  do{
     do{
-      printf("Inserisci la password: ");
-      ottieni_stringa(password, DIMENSIONE_STRINGA_PASSWORD);
+      do{
+        printf("Inserisci la password: ");
+        ottieni_stringa(password, DIMENSIONE_STRINGA_PASSWORD);
 
-      lvl = controllo_password(password);
-    }while(risposta_password(lvl) < 0);
+        lvl = controllo_password(password);
+      }while(risposta_password(lvl) < 0);
 
-    printf("Conferma Password\n");
-    ottieni_stringa(password_2, DIMENSIONE_PASSWORD);
-    conferma = strcmp(password, password_2) != 0;
-    if(conferma){
-      printf("Le password non corrispondono\n");
+      printf("Conferma Password\n");
+      ottieni_stringa(password_2, DIMENSIONE_PASSWORD);
+      conferma = strcmp(password, password_2) != 0;
+      if(conferma){
+        printf("Le password non corrispondono\n");
+      }
+    }while(conferma);
+
+    md5(password, strlen(password), password_mod);
+    utente = crea_utente(email, password_mod, nome, cognome, permesso);
+
+    if(aggiungi_utente_in_tabella(tabella_utenti, utente)){
+        return 1;
     }
-  }while(conferma);
 
-  md5(password, strlen(password), password_mod);
-  utente = crea_utente(email, password_mod, nome, cognome, permesso);
+    return 0;
+}
 
-  if(aggiungi_utente_in_tabella(tabella_utenti, utente)){
-    return 1;
-  }
-  return 0;
+Intervallo richiedi_intervallo_prenotazione(Intervallo i){
+      char inizio[DIMENSIONE_INTERVALLO];
+      char fine[DIMENSIONE_INTERVALLO];
+
+      do{
+          printf("Inserisci il giorno, il mese, l'anno e l'orario iniali ");
+          printf("secondo questo formato: dd/mm/yyyy HH:MM: ");
+          fgets(inizio, DIMENSIONE_INTERVALLO, stdin);
+          inizio[strlen(inizio) - 1] = '\0';
+
+          printf("Inserisci il giorno, il mese, l'anno e l'orario finali ");
+          printf("secondo questo formato: dd/mm/yyyy HH:MM: ");
+          fgets(fine, DIMENSIONE_INTERVALLO, stdin);
+          fine[strlen(fine) - 1] = '\0';
+
+          i = converti_data_in_intervallo(inizio, fine);
+
+          if(i==NULL || inizio_intervallo(i) < time(NULL)) printf("Intervallo non valido\n");
+
+          printf("Vuoi uscire? (S/N): ");
+          scelta = getchar();
+
+          if(scelta == 's' || scelta == 'S'){
+            return NULL;
+          }
+      }while(i == NULL);
+
+      return i;
 }
