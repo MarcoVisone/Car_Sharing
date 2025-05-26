40d61f5c40ac4dbb50351584803c4250f755bb7a
Manuel. <russo.nello.manuel@gmail.com>
2025-05-26 19:10:27 +0200

    Aggiunto commento per interfaccia_rimuovi_veicolo

--- include/interfaccia/interfaccia_amministratore.h
+++ include/interfaccia/interfaccia_amministratore.h
@@ -18,6 +18,28 @@ Byte gestione_noleggi(TabellaVeicoli tabella_veicoli);
 
 Veicolo interfaccia_aggiungi_veicolo();
 
+/*
+ * Autore: Russo Nello Manuel
+ * Data: 26/05/2025
+ *
+ * Funzione: interfaccia_rimuovi_veicolo
+ * -------------------------------------
+ * Interfaccia testuale che consente all’utente di rimuovere un veicolo dalla tabella
+ *
+ * Parametri:
+ *    - tabella_veicoli: la tabella veicoli
+ *
+ * Pre-condizioni:
+ *    tabella_veicoli: non deve essere NULL
+ *
+ * Post-condizioni:
+ * 	  restituisce 0 in caso di uscita o fallimento nella rimozione.
+ *
+ * Side-effects:
+ *    - Input/output su console
+ *    - Allocazione e deallocazione dinamica della memoria
+ *    - Il veicolo selezionato, se presente e confermato, viene rimosso dalla tabella.
+ */
 Byte interfaccia_rimuovi_veicolo(TabellaVeicoli tabella_veicoli);
 
 void visualizza_veicoli(TabellaVeicoli tabella_veicoli);
