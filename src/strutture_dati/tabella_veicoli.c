25af074676ce09be6a4489c75eea1c0007139898
Manuel. <russo.nello.manuel@gmail.com>
2025-05-29 18:04:55 +0200

    Modificati i commenti

--- src/strutture_dati/tabella_veicoli.c
+++ src/strutture_dati/tabella_veicoli.c
@@ -29,6 +29,9 @@
  *    restituisce un oggetto TabellaVeicoli inizializzato,
  *    oppure NULL in caso di errore di allocazione
  *
+ * Ritorna:
+ *    un oggetto TabellaVeicoli o NULL
+ *
  * Side-effect:
  *    Alloca dinamicamente memoria per la struttura tabella hash
  */
@@ -50,6 +53,9 @@ TabellaVeicoli crea_tabella_veicoli(unsigned int grandezza){
  *    veicolo: non deve essere NULL
  *
  * Post-condizioni:
+ *    non restituisce niente
+ *
+ * Side-effect:
  *    la memoria associata al veicolo viene liberata
  */
 static void distruggi_veicolo_t(void *veicolo){
@@ -114,6 +120,9 @@ void distruggi_tabella_veicoli(TabellaVeicoli tabella_veicoli){
  * Post-condizioni:
  *    restituisce 1 se l'inserimento è avvenuto con successo, 0 altrimenti
  *
+ * Ritorna:
+ *    un valore di tipo Byte(1 oppure 0)
+ *
  * Side-effect:
  *    modifica la tabella hash aggiungendo il veicolo
  */
@@ -135,7 +144,7 @@ Byte aggiungi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, Veicolo veicolo
  *
  * Parametri:
  *    tabella_veicoli: puntatore alla tabella hash dei veicoli
- *    targa: stringa contenente la targa del veicolo da cercare
+ *    targa: stringa costante contenente la targa del veicolo da cercare
  *
  * Pre-condizioni:
  *    tabella_veicoli: non deve essere NULL
@@ -144,6 +153,9 @@ Byte aggiungi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, Veicolo veicolo
  * Post-condizioni:
  *    restituisce un puntatore al veicolo se presente nella tabella,
  *    altrimenti restituisce NULL
+ *
+ * Ritorna:
+ *    un puntatore ad un veicolo o NULL
  */
 Veicolo cerca_veicolo_in_tabella(const TabellaVeicoli tabella_veicoli, const char *targa){
     if(tabella_veicoli == NULL || targa == NULL) return NULL;
@@ -172,6 +184,9 @@ Veicolo cerca_veicolo_in_tabella(const TabellaVeicoli tabella_veicoli, const cha
  * Post-condizioni:
  *    restituisce 1 se il veicolo è stato rimosso correttamente,
  *    0 se la targa non è presente o si è verificato un errore
+ *
+ * Ritorna:
+ *    un valore di tipo Byte(1 oppure 0)
  *
  * Side-effect:
  *    Modifica la tabella dei veicoli rimuovendo l'elemento specificato
@@ -204,10 +219,10 @@ Byte rimuovi_veicolo_in_tabella(TabellaVeicoli tabella_veicoli, char *targa){
  *    dimensione: non deve essere NULL
  *
  * Post-condizioni:
- *    Restituisce un array allocato dinamicamente di puntatori a veicoli:
- *    - i veicoli occupati nell'intervallo vengono posti a NULL
- *    - quelli disponibili mantengono il puntatore originale
- *    - restituisce NULL in caso di errore
+ *    Restituisce un array allocato dinamicamente di puntatori a veicoli, restituisce NULL in caso di errore
+ *
+ * Ritorna:
+ *    un array di veicoli o NULL
  *
  * Side-effect:
  *    Alloca dinamicamente un array che dovrà essere liberato dal chiamante.
